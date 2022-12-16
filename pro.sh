25f7e5e2d96cd5289df09116329e0fedd3ae16af
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-16 09:17:25 +0800

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-sudo -i || su root 
-yum install sudo git wget vim && apt install sudo git wget vim 
-sudo rm -rf /root/1 || sudo rm -rf /var/1 
-git clone https://github.com/xvmvx/1.git /var/1
-chmod +x /var/1/*
-source /var/1/1.sh
+sudo -i
+if [ "$?" = "0" ]; then
+  echo "欢迎你！！！"
+else
+  su root 
+  apt install sudo git wget vim || yum install sudo git wget vim
+  sudo -i
+fi
+FILE=/var/1
+if [ -e "$FILE" ]; then
+    echo "$FILE 已经存在，是否更新？"
+    read -p ">>>>>>>>>>>>>>>>" character
+      if [ "$character" = "y" ]; then
+        sudo rm -rf /var/1 && git clone https://github.com/xvmvx/1.git /var/1
+      elif [ "$character" = "n" ]; then
+        chmod +x /var/1/* && source /var/1/1.sh
+      fi
+fi
+
