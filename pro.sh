ac13f2589680fe9377b1e1faf4a253857dacaa3a
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-16 22:28:45 +0800

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -8,23 +8,12 @@ else
   sudo -i
 fi
 
-  echo -n " 是否更新 1 文件？ "
-  read -p ">>>>>>>>>>>>>>>>" character1
-if [ "$character1" = "y" ]; then
-     rm -rf /root/1 && rm -rf /var/1
-     git clone https://github.com/xvmvx/1.git /var/1
-     cd /var/1 && chmod +x *
-fi
-if [ "$?" = "0" ]; then  
-  ./1.sh
-fi
-
 FILE=/var/1
 if [ -e "$FILE" ]; then
     echo "$FILE 已经存在，是否更新？"
     read -p ">>>>>>>>>>>>>>>>" character
       if [ "$character" = "y" ]; then
-        sudo rm -rf /var/1 && git clone https://github.com/xvmvx/1.git /var/
+        sudo rm -rf /var/1 && git clone https://github.com/xvmvx/1.git /var/1
       elif [ "$character" = "n" ]; then
         chmod +x /var/1/* && source /var/1/1.sh
       fi
