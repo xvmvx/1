3defaa488526656e51b575d3bcb0d35a1ea16923
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-16 09:31:23 +0800

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -12,7 +12,7 @@ if [ -e "$FILE" ]; then
     echo "$FILE 已经存在，是否更新？"
     read -p ">>>>>>>>>>>>>>>>" character
       if [ "$character" = "y" ]; then
-        sudo rm -rf /var/1 && git clone https://github.com/xvmvx/1.git /var/1
+        sudo rm -rf /var/1 && git clone https://github.com/xvmvx/1.git /var/
       elif [ "$character" = "n" ]; then
         chmod +x /var/1/* && source /var/1/1.sh
       fi
