3516e28a46938b6bbf49dcde89bc09629676ebd1
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-16 20:08:04 +0800

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -7,6 +7,18 @@ else
   apt install sudo git wget vim || yum install sudo git wget vim
   sudo -i
 fi
+
+  echo -n " 是否更新 1 文件？ "
+  read -p ">>>>>>>>>>>>>>>>" character1
+if [ "$character1" = "y" ]; then
+     rm -rf /root/1 && rm -rf /var/1
+     git clone https://github.com/xvmvx/1.git /var/1
+     cd /var/1 && chmod +x *
+fi
+if [ "$?" = "0" ]; then  
+  ./1.sh
+fi
+
 FILE=/var/1
 if [ -e "$FILE" ]; then
     echo "$FILE 已经存在，是否更新？"
