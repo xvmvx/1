e1186843dcc1cad3af7254d812dca67304147a7d
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-18 01:15:43 +0800

    Update xswitch.sh

--- xswitch.sh
+++ xswitch.sh
@@ -40,14 +40,19 @@ else
     echo 输入不符合要求
 fi
 make up
-echo "http://你的服务器IP:8081 访问Web界面。默认用户名和密码都是admin。也可通过执行docker ps可查看 docker 服务是否已正常启动。
-如果有服务启动出错，可通过执行docker logs -f xswitch查看xswitch容器相关日志（xswitch为容器名）。"
-echo " 通过执行make bash进入xswitch容器内部，然后输入fs_cli可以连接控制台查看系统运行情况。"
-echo " 通过执行make up-nginx  可启用Nginx "
-echo "    "
-echo "    "
-echo "    "
-echo "进入 XSwitch 容器 执行    make bash  "
-echo "进入 XSwitch 控制台   执行make cli  "
-echo "进入数据库   执行make bash-pg   "
-echo "进入nginx容器   执行make bash-nginx   "
+if [ "$?" = "0" ]; then
+  echo "已安装成功！"
+  echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+  echo "  " 
+  echo -n " 访问Web界面     http://"; echo -n ${IP2}；echo ":8081" 
+  echo " Web界面用户名：  admin  密码：admin"
+  echo " 查看日志：       docker logs -f 容器名"
+  echo " fs_cli控制台：   make bash"
+  echo " XSwitch控制台：  make cli"
+  echo " 查看数据库：      make bash-pg"
+  echo " 启用Nginx：      make up-nginx"
+  echo " Nginx数据库：    make bash-nginx"
+else
+  echo "安装失败请检查！" 1>&2
+  exit 1
+fi
