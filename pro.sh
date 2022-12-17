ea1dc9b00d2e2cb0a04e9a793c26d23a4ed21651
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-17 23:09:51 +0800

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-sudo -i
-if [ "$?" = "0" ]; then
-  echo "欢迎你！！！"
-else
-  su root 
-  apt install sudo git wget vim || yum install sudo git wget vim
-  sudo -i
-fi
-
 FILE=/var/1
 if [ -e "$FILE" ]; then
     echo "$FILE 已经存在，是否更新？"
